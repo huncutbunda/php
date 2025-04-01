ade8462401e9dcf7a5228b05bee4923bf414ea88
szaboakos <100764787+huncutbunda@users.noreply.github.com>
2025-04-01 07:06:07 +0200

    Add files via upload
    SQL file attached.

---
+++ vizs.sql
@@ -0,0 +1,180 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Gép: 127.0.0.1
+-- Létrehozás ideje: 2025. Már 20. 12:12
+-- Kiszolgáló verziója: 10.4.32-MariaDB
+-- PHP verzió: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Adatbázis: `vizs`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `brands`
+--
+
+CREATE TABLE `brands` (
+  `BrandId` int(10) NOT NULL,
+  `BrandName` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `cars`
+--
+
+CREATE TABLE `cars` (
+  `CarId` int(10) NOT NULL,
+  `Model` varchar(100) NOT NULL,
+  `Horsepower` varchar(7) NOT NULL,
+  `Color` varchar(10) NOT NULL,
+  `Price` int(11) NOT NULL,
+  `BrandId` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `motors`
+--
+
+CREATE TABLE `motors` (
+  `MotorId` int(10) NOT NULL,
+  `Model` varchar(100) NOT NULL,
+  `Horsepower` varchar(7) NOT NULL,
+  `Color` varchar(10) NOT NULL,
+  `Price` int(11) NOT NULL,
+  `BrandId` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `users`
+--
+
+CREATE TABLE `users` (
+  `UserId` int(10) NOT NULL,
+  `Username` varchar(100) NOT NULL,
+  `Password` varchar(10) NOT NULL,
+  `Email` varchar(100) NOT NULL,
+  `Number` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `regusers`
+--
+
+CREATE TABLE `regusers` (
+  `reguserid` int(10) NOT NULL,
+  `regpass` varchar(100) NOT NULL,
+  `regemail` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `regusers`
+--
+
+CREATE TABLE `logusers` (
+  `loguserid` int(10) NOT NULL,
+  `logpass` varchar(100) NOT NULL,
+  `logemail` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexek a kiírt táblákhoz
+--
+
+--
+-- A tábla indexei `brands`
+--
+ALTER TABLE `brands`
+  ADD PRIMARY KEY (`BrandId`);
+
+--
+-- A tábla indexei `cars`
+--
+ALTER TABLE `cars`
+  ADD PRIMARY KEY (`CarId`),
+  ADD KEY `BrandId` (`BrandId`);
+
+--
+-- A tábla indexei `motors`
+--
+ALTER TABLE `motors`
+  ADD PRIMARY KEY (`MotorId`),
+  ADD KEY `BrandId` (`BrandId`);
+
+--
+-- A tábla indexei `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`UserId`);
+
+--
+-- A kiírt táblák AUTO_INCREMENT értéke
+--
+
+--
+-- AUTO_INCREMENT a táblához `brands`
+--
+ALTER TABLE `brands`
+  MODIFY `BrandId` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT a táblához `cars`
+--
+ALTER TABLE `cars`
+  MODIFY `CarId` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT a táblához `motors`
+--
+ALTER TABLE `motors`
+  MODIFY `MotorId` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT a táblához `users`
+--
+ALTER TABLE `users`
+  MODIFY `UserId` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- Megkötések a kiírt táblákhoz
+--
+
+--
+-- Megkötések a táblához `cars`
+--
+ALTER TABLE `cars`
+  ADD CONSTRAINT `cars_ibfk_1` FOREIGN KEY (`BrandId`) REFERENCES `brands` (`BrandId`);
+
+--
+-- Megkötések a táblához `motors`
+--
+ALTER TABLE `motors`
+  ADD CONSTRAINT `motors_ibfk_1` FOREIGN KEY (`BrandId`) REFERENCES `brands` (`BrandId`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
