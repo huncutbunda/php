257e9075286aac6c0968bb63d93f1218255b6ab0
Rendszergazda <rendszergazda@blathy.info>
2025-04-15 09:37:36 +0200

    Vue scaffolding

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -19,3 +19,11 @@ Route::get('user/{id}', function ($id) {
 Route::get('/about', function () {
     return view('about', ['name' => '123']);
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
