38917b90afe8f242e75cd69ce2aebb45840d2664
Szabó Ákos Csaba <szabo.akos.csaba@blathy.info>
2025-04-30 15:48:30 +0200

    Model creation

---
+++ example-app/app/Http/Controllers/MotorsController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Motors;
+use App\Http\Requests\StoreMotorsRequest;
+use App\Http\Requests\UpdateMotorsRequest;
+
+class MotorsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreMotorsRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Motors $motors)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Motors $motors)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateMotorsRequest $request, Motors $motors)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Motors $motors)
+    {
+        //
+    }
+}
